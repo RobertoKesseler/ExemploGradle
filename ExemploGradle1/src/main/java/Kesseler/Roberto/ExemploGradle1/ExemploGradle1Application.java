4194feb8386e001390bc4c627f9966cc454d8c5f
Roberto Kesseler <r.kesselerkesseler@gmail.com>
2020-04-19 18:00:42 -0300

    Exemplo SpringBoot Gradle

---
+++ ExemploGradle1/src/main/java/Kesseler/Roberto/ExemploGradle1/ExemploGradle1Application.java
@@ -0,0 +1,16 @@
+package Kesseler.Roberto.ExemploGradle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradle1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExemploGradle1Application.class, args);
+
+		System.out.println("Hello World SpringBoot com Gradle");
+	}
+
+}
---
+++ ExemploGradle1/src/test/java/Kesseler/Roberto/ExemploGradle1/ExemploGradle1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Kesseler.Roberto.ExemploGradle1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradle1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
